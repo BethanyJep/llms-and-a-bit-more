2364a741dd85feab7addb6eaa9084288c4d35ff0
bethanyjep <bethanycheum@microsoft.com>
2025-09-02 16:37:10 +0300

    updated client-llm.py file

--- mcp-bootcamp/client-llm.py
+++ mcp-bootcamp/client-llm.py
@@ -1,12 +1,93 @@
 from mcp import ClientSession, StdioServerParameters, types
 from mcp.client.stdio import stdio_client
 
+import os
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+import json
+
 # Create server parameters for stdio connection
 server_params = StdioServerParameters(
     command="mcp",  # Executable
-    args=["run", "server.py"],  # Optional command line arguments
+    args=["run", "mcp-bootcamp/server.py"],  # Optional command line arguments
     env=None,  # Optional environment variables
 )
+
+def call_llm(prompt, functions):
+    token = os.environ["GITHUB_TOKEN"]
+    endpoint = "https://models.inference.ai.azure.com"
+
+    model_name = "openai/gpt-4o"
+
+    client = OpenAI(
+        endpoint=endpoint,
+        credential=token,
+    )
+
+    print("CALLING LLM")
+    response = client.complete(
+        messages=[
+            {
+            "role": "system",
+            "content": "You are a helpful assistant.",
+            },
+            {
+            "role": "user",
+            "content": prompt,
+            },
+        ],
+        model=model_name,
+        tools = functions,
+        # Optional parameters
+        temperature=1.,
+        max_tokens=1000,
+        top_p=1.    
+    )
+
+    response_message = response.choices[0].message
+    
+    functions_to_call = []
+
+    if response_message.tool_calls:
+        for tool_call in response_message.tool_calls:
+            print("TOOL: ", tool_call)
+            name = tool_call.function.name
+            args = json.loads(tool_call.function.arguments)
+            functions_to_call.append({ "name": name, "args": args })
+
+    return functions_to_call
+
+def convert_to_llm_tool(tool):
+    tool_schema = {
+        "type": "function",
+        "function": {
+            "name": tool.name,
+            "description": tool.description,
+            "type": "function",
+            "parameters": {
+                "type": "object",
+                "properties": tool.inputSchema["properties"]
+            }
+        }
+    }
+
+    return tool_schema
+
+def convert_to_llm_tool(tool):
+    tool_schema = {
+        "Type": "function",
+        "function": {
+            "name": tool.name,
+            "description": tool.description,
+            "parameters": {
+                "type": "object",
+                "properties": tool.inputSchema["properties"],
+            }
+        }
+    }
+
+    return tool_schema
 
 async def run():
     async with stdio_client(server_params) as (read, write):
